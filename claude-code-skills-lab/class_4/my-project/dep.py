a1976122f30aadf4f3c6709caa34bbe3884f3527
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-16 04:29:05 +0500

    updated dependence injection code

--- claude-code-skills-lab/class_4/my-project/dep.py
+++ claude-code-skills-lab/class_4/my-project/dep.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, Depends
 from dotenv import load_dotenv
 import os
+
 app = FastAPI()
 
 load_dotenv()
 
-
 def get_config():
     print("\n CONFIG FUNC: 1")
-    return {"app": "taskapi", "gemini-key": os.getenv("GEMINI_API_KEY")}
+    return {"app": "taskapi", "gemini_key": os.getenv("GEMINI_API_KEY")}
 
 @app.get("/hello")
 def hello(config: dict = Depends(get_config)):
     print("\n NORMAL API FUNC: 1")
     # config = get_config()
-    return {"message" : "all good", "gemini-key": config["geminikey"]}
+    return {"message" : "all good", "gemini_key": config["gemini_key"]}
