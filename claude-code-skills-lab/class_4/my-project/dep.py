34ad6fac3e41b171652c422cdb69a922681ae070
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-16 00:07:01 +0500

    update dependence injection code

--- claude-code-skills-lab/class_4/my-project/dep.py
+++ claude-code-skills-lab/class_4/my-project/dep.py
@@ -5,11 +5,13 @@ from fastapi import FastAPI, Depends
 app = FastAPI()
 
 def get_config():
+    print("\n CONFIG FUNC: 1")
     return {"app": "taskapi", "storage": "in-memory"}
 
 
 @app.get("/hello")
 def hello(config: dict = Depends(get_config)):
+    print("\n NORMAL API FUNC: 1")
     # config = get_config()
     return {"message" : "all good", "app-name": config["app"]}
 
