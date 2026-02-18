af69c8a6ea0dc8d643b9b5aae6a9462ef90dc1cd
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-18 06:55:35 +0500

    update main.py creating sqlmodel

--- claude-code-skills-lab/class_4/my-project/main.py
+++ claude-code-skills-lab/class_4/my-project/main.py
@@ -3,10 +3,13 @@ from sqlmodel import SQLModel, Field
 
 app = FastAPI()
 
+# DB Structure/Tables + Same used at API level
 class Task(SQLModel, table=True):
      id: int | None = Field(default=None, primary_key=True)
      title: str
      description: str | None = Field(default=None)
+    
+# DB Configuration    
     
 @app.post("/tasks")
 def create_task(task: Task):
