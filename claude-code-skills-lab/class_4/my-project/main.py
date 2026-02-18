bf724f8623071d76780e2cfdf17dc75aa00be9c9
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-18 06:58:51 +0500

    working in main.py creating sqlmodel

--- claude-code-skills-lab/class_4/my-project/main.py
+++ claude-code-skills-lab/class_4/my-project/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, create_engine, Session
 from dotenv import load_env
 import os
 
 load_env()
+
+engine = create_engine(os.getenv("DB_URL"))
 
 app = FastAPI()
 
