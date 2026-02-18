45bfc3bf086c835281aa255f5ea9063015c2b2cc
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-18 06:53:48 +0500

    update main.py creating sqlmodel

--- claude-code-skills-lab/class_4/my-project/main.py
+++ claude-code-skills-lab/class_4/my-project/main.py
@@ -3,9 +3,11 @@ from sqlmodel import SQLModel, Field
 
 app = FastAPI()
 
-class Task(SQLModel):
+class Task(SQLModel, table=True):
+     id: int | None = Field(default=None, primary_key=True)
+     title: str
+     description: str | None = Field(default=None)
     
-
-@app.get("/hello")
-def hello():
+@app.post("/tasks")
+def create_task(task: Task):
     return {"message": "all good"}
