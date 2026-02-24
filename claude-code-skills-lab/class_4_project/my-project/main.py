5e2d290a523f90ae44e364c83bb4f05222eb4967
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-02-24 18:12:01 +0500

    trying to create real database

--- claude-code-skills-lab/class_4_project/my-project/main.py
+++ claude-code-skills-lab/class_4_project/my-project/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI, HTTPException
-from sqlmodel import SQLModel, Field, create_engine, Session, select
-import os
+from sqlmodel import SQLModel, Field, Session, select, create_engine
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-engine = create_engine(os.getenv("DB_URL"), echo=True)
-print(engine)
-print(os.getenv("DB_URL"))
-
 app = FastAPI(
     title="Task API",
-    description="A simple task management api"
-)
+    description="A simple task management app"
+    )
+
+engine = create_engine(os.getenv("DB_URL"), echo=True)
+print(engine)
+
 
 # Model
 
@@ -21,15 +21,9 @@ class Task(SQLModel, table=True):
     title: str
     description: str
     completed: bool = False
-
-# Create Table
-
-@app.on_event("startup")
-def on_startup():
-    SQLModel.metadata.create_all(engine)
-
+    
 # Create Task
-
+    
 @app.post("/tasks")
 def create_task(task: Task):
     with Session(engine) as session:
@@ -37,69 +31,31 @@ def create_task(task: Task):
         session.commit()
         session.refresh(task)
         return task
-
-# Read All Task
+    
+# Get all task    
+    
+@app.get("/tasks")
+def get_tasks(task: Task):
+    with Session(engine) as session:
+        session.exec(select(Task)).all()
+        return task
+    
+# Get single task
 
 @app.get("/tasks")
-def get_tasks():
+def get_task(task: Task, task_id: int):
     with Session(engine) as session:
-        tasks = session.exec(select(Task)).all()
-        return tasks
-        
-
-# Read Single Task
-
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-    with Session(engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
+        tasks = session.get(Task, task_id)
+        if not tasks
             raise HTTPException(status_code=404, detail="Task not found")
         return task
-    
-    
-# Update Task
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int, updated_task: Task):
-    with Session(engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=404, detail="Task not found")
-        
-        task.title = updated_task.title
-        task.description = updated_task.description
-        task.completed = updated_task.completed
-        
-        session.add(task)
-        session.commit()
-        session.refresh(task)
-        return task
-        
     
-# Patch (Mark Complete)
+    
+# Create Table
 
-@app.patch("/tasks/{task_id}")
-def complete_task(task_id: int):
-    with Session(engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=404, detail="Task not found")
-    
-        task.completed = True
-        task.commit()
-        task.refresh()
-        return task 
-    
-# Delete Task
+def create_table():
+    SQLModel.metadata.create_all(engine)
 
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    with Session(engine) as session:
-        task = session.get(Task, task_id)
-        if not task:
-            raise HTTPException(status_code=404, detail="Task not found")
-        
-        session.delete(task)
-        session.commit()
-        return {"message": "Task deleted successfully"}
+
+create_table()
