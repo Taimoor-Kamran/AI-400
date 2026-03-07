1c644be601c5c9a21b6ec09ef2992f9819679b17
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-03-07 23:23:15 +0500

    class 5 folder creates

--- claude-code-skills-lab/class_5/my-project/main.py
+++ claude-code-skills-lab/class_5/my-project/main.py
@@ -1,6 +1,53 @@
-def main():
-    print("Hello from my-project!")
+
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+engine = create_engine(os.getenv("DB_URL"), echo=True)
+
+# DB Structure/Tables + Same used at API level
+class Task(SQLModel, table=True):
+     id: int | None = Field(default=None, primary_key=True)
+     title: str
+     description: str | None = Field(default=None)
+
+# Migration Command
+# SQLModel.metadata.create_all(engine)
 
 
-if __name__ == "__main__":
-    main()
+# How to actually interact with tables?
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+app = FastAPI()
+
+# DB Configuration
+
+@app.post("/tasks")
+def create_task(task: Task, session: Session = Depends(get_session)):
+    session.add(task)
+    session.commit()
+    return task
+
+@app.get("/tasks")
+def get_tasks(session: Session = Depends(get_session)):
+    tasks = session.exec(select(Task)).all()
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int, session: Session = Depends(get_session)):
+    task = session.get(Task, task_id)
+    return task
+
+# How to create Table?
+# def create_tables():
+#     print("trying to create table")
+#     SQLModel.metadata.create_all(engine)
+#     print("Tables Function Completed")
+
+# create_tables()
