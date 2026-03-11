d53ec671a670ca572b9e7b45ac949f21b8b73fb0
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-03-11 16:34:19 +0500

    now added some code

--- claude-code-skills-lab/class_5/my-project/hash.py
+++ claude-code-skills-lab/class_5/my-project/hash.py
@@ -8,11 +8,9 @@ def hash_password(password: str) -> str:
     """Hash a password with Argon2."""
     return password_hash.hash(password)
 
-print(hash_password("taimoor_345"))
-print(hash_password("saqlain_345"))
-print(hash_password("yousuf_345"))
-
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a password against its hash."""
     return password_hash.verify(plain_password, hashed_password)
+
+print(verify_password("taimoor_345", hashed_password="taimoor_345"))
