c44e2113cf5571d1e912cb9450e620cc6822d910
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-03-11 16:29:34 +0500

    created new file hash.py

---
+++ claude-code-skills-lab/class_5/my-project/hash.py
@@ -0,0 +1,18 @@
+from pwdlib import PasswordHash
+from pwdlib.hashers.argon2 import Argon2Hasher
+
+password_hash = PasswordHash((Argon2Hasher(),))
+
+
+def hash_password(password: str) -> str:
+    """Hash a password with Argon2."""
+    return password_hash.hash(password)
+
+print(hash_password("taimoor_345"))
+print(hash_password("saqlain_345"))
+print(hash_password("yousuf_345"))
+
+
+# def verify_password(plain_password: str, hashed_password: str) -> bool:
+#     """Verify a password against its hash."""
+#     return password_hash.verify(plain_password, hashed_password)
