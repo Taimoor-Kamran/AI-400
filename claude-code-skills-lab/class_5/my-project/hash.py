30933ca90ef48f6a0714f8bc0433ec82a7f94ef2
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-03-11 16:36:48 +0500

    now added some code

--- claude-code-skills-lab/class_5/my-project/hash.py
+++ claude-code-skills-lab/class_5/my-project/hash.py
@@ -13,4 +13,4 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     """Verify a password against its hash."""
     return password_hash.verify(plain_password, hashed_password)
 
-print(verify_password("taimoor_345", hashed_password="taimoor_345"))
+print(verify_password("taimoor_345", hash_password("taimoor_345")))
