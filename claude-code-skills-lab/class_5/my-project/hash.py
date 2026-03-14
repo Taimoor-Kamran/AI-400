c904a6e63a640826fa4647d62b438d76ac0be1d0
Taimoor Kamran <muhammadtaimoor2006@gmail.com>
2026-03-14 23:46:53 +0500

    adding jwt code

--- claude-code-skills-lab/class_5/my-project/hash.py
+++ claude-code-skills-lab/class_5/my-project/hash.py
@@ -22,3 +22,5 @@ def decode_token(token: str) -> Optional[dict]:
         return None
     
 token = create_access_token({"sub": "junaid@gmail.com"})
+print("\n[+] Token: ", token)
+print("\n[+] Decoded Token: ", decode_token(token))
